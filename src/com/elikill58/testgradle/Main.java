b68b8041e5e5f6cb979592963762c0714b145ca1
Elikill58 <arpetzouille@gmail.com>
2024-09-28 15:13:01 +0200

    Initial commit

---
+++ src/com/elikill58/testgradle/Main.java
@@ -0,0 +1,10 @@
+package com.elikill58.testgradle;
+
+import com.elikill58.test.GradleIssue;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+		GradleIssue.checkIfIssue("");
+	}
+}
